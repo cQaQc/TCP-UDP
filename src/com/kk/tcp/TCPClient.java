d8b2c0aba8d8bfea61378e2e9ce629f944ac69cb
蜘蛛啊全是腿儿 <446783737@qq.com>
2020-11-23 16:24:13 +0800

    服务端实现消息累计接收

--- src/com/kk/tcp/TCPClient.java
+++ src/com/kk/tcp/TCPClient.java
@@ -16,16 +16,29 @@ public class TCPClient {
     */
     public static void main(String[] args) throws IOException {
 
-        //1.客户端==>我需要知道服务器的地址
-        Socket socket = new Socket("127.0.0.1", 9999);
-        //2.发送消息
-        OutputStream outputStream = socket.getOutputStream();
-        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-        dataOutputStream.writeUTF("欢迎您，柯神_");
-        //最后关闭连接，释放资源
-        dataOutputStream.close();
-        outputStream.close();
-        socket.close();
+        Socket socket = null;
+        OutputStream outputStream = null;
+        DataOutputStream dataOutputStream=null;
 
+        //1.客户端==>我需要知道服务器的地址
+        try {
+            socket = new Socket("127.0.0.1", 9999);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //2.发送消息
+            try {
+                outputStream = socket.getOutputStream();
+                dataOutputStream = new DataOutputStream(outputStream);
+                dataOutputStream.writeUTF("欢迎您，柯神_");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                //最后关闭连接，释放资源
+                dataOutputStream.close();
+                outputStream.close();
+                socket.close();
+            }
     }
 }
--- src/com/kk/tcp/TCPServer.java
+++ src/com/kk/tcp/TCPServer.java
@@ -16,17 +16,31 @@ public class TCPServer {
 */
     public static void main(String[] args) throws IOException {
 
+        Socket accept = null;
+        InputStream inputStream = null;
+        DataInputStream dataInputStream = null;
+
         //1.服务端==>我得有一个地址，提供给客户端
-        ServerSocket serverSocket = new ServerSocket(9999);
-        //2.等待客户端的连接
-        Socket accept = serverSocket.accept();
-        //3.读取消息
-        InputStream inputStream = accept.getInputStream();
-        DataInputStream dataInputStream = new DataInputStream(inputStream);
-        System.out.println(dataInputStream.readUTF());
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(9999);
+
+            //2.等待客户端的连接
+            while (true) {
+                accept = serverSocket.accept();
+                //3.读取消息
+                inputStream = accept.getInputStream();
+                dataInputStream = new DataInputStream(inputStream);
+                System.out.println(dataInputStream.readUTF());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            dataInputStream.close();
+            inputStream.close();
+            accept.close();
+        }
+
         //最后关闭连接，释放资源
-        dataInputStream.close();
-        inputStream.close();
-        accept.close();
     }
 }
